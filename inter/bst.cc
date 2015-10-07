49aa59b30cd3288f36238d2244a95ef16ba61e3c
Chinu <chinumontu@yahoo.com>
2015-10-06 17:17:20 -0700

    Find minimum in a range

--- inter/bst.cc
+++ inter/bst.cc
@@ -80,7 +80,51 @@ void test_findSecondLargest()
 	assert(findSecondLargest(root) == 25);
 }
 
+
+//
+// Find minimun value in a given range from BST
+//
+const Node* findMinInRange(const Node* root, int min, int max)
+{
+	if (root == nullptr) return nullptr;
+	//cout <<  "iterating : " << root->value << " with min/max " << min << " " << max << endl;
+
+
+	// Check left sub-tree to find min value
+	if (root->value > min) {
+		const Node *minNode = findMinInRange(root->left, min, max);
+		if (minNode != nullptr) return minNode;
+	}
+
+	// Check if this node is the answer
+	if (root->value >= min and root->value <= max) return root;
+
+	// Check right sub-tree for answer
+	if (root->value < max)return findMinInRange(root->right, min, max);
+
+	return nullptr;
+}
+
+void test_findMin()
+{
+	Node *root = new Node(10);
+	root->left = new Node(5);
+	root->left->left = new Node(3);
+	root->left->right = new Node(8);
+	root->right = new Node(15);
+	root->right->left = new Node(12);
+	root->right->right = new Node(18);
+
+	assert(findMinInRange(nullptr,0,0) == nullptr);
+	assert(findMinInRange(root,0,0) == nullptr);
+	assert(findMinInRange(root,1,2) == nullptr);
+	assert(findMinInRange(root,1,3)->value == 3);
+	assert(findMinInRange(root,7,13)->value == 8);
+}
+
+
 int main()
 {
 	test_findSecondLargest();
+	test_findMin();
 }
