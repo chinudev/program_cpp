88b6f9036358115bb8e5307fc48344ad700090b4
chindev <chinumontu@yahoo.com>
2021-02-28 07:10:44 -0800

    Update to solve the follow-up problem.

--- coding_interview/dynamic/robot_ways_2dgrid.cpp
+++ coding_interview/dynamic/robot_ways_2dgrid.cpp
@@ -14,8 +14,14 @@ unsigned int findNumPaths(unsigned int X, unsigned int Y)
     numPaths[0][0] = 1;
     for (int x=0; x <=X; x++) {
         for (int y=0; y <=Y; y++) {
-            if (x < X) numPaths[x+1][y] += numPaths[x][y];
-            if (y < Y) numPaths[x][y+1] += numPaths[x][y];
+            if (numPaths[x][y] > 0) { 
+                if ((numPaths[x+1][y] >= 0) && (x < X)) { 
+                    numPaths[x+1][y] += numPaths[x][y];
+                }
+                if ((numPaths[x][y+1] >= 0) && (y < Y)) { 
+                    numPaths[x][y+1] += numPaths[x][y];
+                }
+            }
         }
     }
     return numPaths[X][Y];
